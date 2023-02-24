0ec3d77f561f6be34527480c40f428bc7cf082b2
trentfridey <tfri.92@gmail.com>
2023-02-24 14:05:57 -0500

    update with form & stuff

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,77 @@
 <template>
-  <main>
-    <h1>Trent Fridey &mdash; Portfolio</h1>
-    <div>
-      <a href="https://github.com/trentfridey">
-        <img src="icons/github.svg" width="24" height="24" style="margin-right: 15px" />
-      </a>
-      <a href="https://linkedin.com/in/trentfridey">
-        <img src="icons/linkedin.svg" width="24" height="24" />
-      </a>
+  <header>
+    <div class="header-inner">
+      <h1>
+        Trent&nbsp;Fridey
+      // software engineer
+      </h1>
+      
     </div>
-  </main>
-  <hr />
+  </header>
   <section>
-  <h2>Personal Projects</h2>
-    <div v-for="project in projects" class="card">
-      <div>
-        <a :href="`${project.site || project.src}`" class="preview">
-          <img :src="`${project.preview}`" class="preview-img" />
-        </a>
-      </div>
-      <div class="card-content">
-        <h2>
-          <a :href="`${project.src}`">{{ project.name }}</a>
-        </h2>
-        <div class="project-description">{{ project.description }}</div>
-        <div class="tools">
-          <div class="tool" v-for="tool in project.tools">
-            <img :src="`icons/${tool}.svg`" width="20" style="margin-right: 8px" />
-            <span class="tool-name">{{ tool }}</span>
+    <div className="cards">
+      <div
+        v-for="(project, idx) in projects"
+        class="card"
+        v-motion
+        :initial="{ opacity: 0, y: 100 }"
+        :enter="{ opacity: 1, y: 0, scale: 1 }"
+        :variants="{ custom: { scale: 2 } }"
+        :delay="200 + 100 * idx"
+      >
+        <div class="card-header">
+          <a :href="`${project.site}`" class="h2">{{ project.name }}</a>
+          <a :href="`${project.src}`"
+            ><img
+              style="vertical-align: text-top"
+              src="icons/github.svg"
+              width="28"
+              height="28"
+          /></a>
+        </div>
+        <div>
+          <a :href="`${project.site || project.src}`" class="preview">
+            <img :src="`${project.preview}`" class="preview-img" />
+          </a>
+        </div>
+        <div class="card-content">
+          <div class="tools">
+            <div class="tool" v-for="tool in project.tools">
+              <img
+                :src="`icons/${tool}.svg`"
+                :alt="tool"
+                width="20"
+                style="vertical-align: text-top; margin-right: 8px"
+              />
+              <span class="tool-name">{{ tool }}</span>
+            </div>
           </div>
+          <div class="project-description">{{ project.description }}</div>
         </div>
       </div>
+    </div>
+  </section>
+  <section id="contact">
+    <div>
+      <h3>Social</h3>
+      <div style="margin: auto">
+        <a href="https://linkedin.com/in/trentfridey" style="color: #efefea"><img
+          style="vertical-align: middle; margin-right: 20px"
+          src="icons/linkedin.svg"
+        /></a>
+        <a href="https://linkedin.com/in/trentfridey" style="color: #efefea">linkedin.com/in/trentfridey</a>
+      </div>
+    </div>
+    <div class="contact-form">
+      <h3>Contact Me</h3>
+      <form data-netlify="true" name="contact" method="post">
+        <input name="form-name" type="hidden" value="contact" />
+        <input name="date" type="hidden" :value="Date()" />
+        <input placeholder="name" name="name" />
+        <input placeholder="email" name="email" />
+        <textarea placeholder="message"></textarea>
+        <button class="submit-btn" type="submit">Submit</button>
+      </form>
     </div>
   </section>
 </template>
@@ -51,35 +92,100 @@ export default {
 </script>
 
 <style lang="scss">
-main {
+:root {
+  --header-height: 120px;
+  --header-inner-height: 120px;
+  font-family: "Inter";
+}
+header {
+  display: flex;
+  align-items: center;
+  position: sticky;
+  height: var(--header-height);
+  top: calc(var(--header-inner-height) - var(--header-height));
+  border-bottom: 1.4px solid black;
+  z-index: 100;
+  background-color: #efefea;
+}
+.header-inner {
+  height: var(--header-inner-height);
+  position: sticky;
+  top: 0;
+  width: 100%;
   display: flex;
   justify-content: space-between;
-  flex-direction: row;
+  padding: 0 20px;
   align-items: center;
-  padding: 20px 20px;
+}
+.header-box {
+  border-radius: 8px;
+  padding: 20px;
+  margin-left: 40px;
 }
 hr {
   margin: 0;
 }
 a {
-  color: black;
+  text-decoration: none;
+  color: rgb(37, 37, 37);
   &:visited {
-    color: black;
+    color: rgb(37, 37, 37);
   }
 }
 h1 {
   font-family: "Inter";
-  font-weight: 500;
+  display: inline;
+  font-weight: 200;
   margin: 0;
-  font-size: 24px;
+  font-size: 64px;
+  color: rgb(37, 37, 37);
+  text-transform: lowercase;
+  letter-spacing: -4px;
+  line-height: 52px;
+  background-color: #efefea;
 }
 h2 {
+  font-family: "Inter";
+  display: inline;
+  font-weight: 300;
   margin: 0;
-  color: black;
+  font-size: 36px;
+  color: rgb(37, 37, 37);
+  text-transform: lowercase;
+  letter-spacing: 0px;
+  line-height: 48px;
+}
+h3 {
+  font-family: "Inter";
+  font-weight: 800;
+  margin: 0;
+  font-size: 18px;
+  color: #efefea;
+  text-transform: lowercase;
+  letter-spacing: -1px;
+  line-height: 30px;
+  background-color: rgb(37, 37, 37);
+}
+.h2 {
+  margin: 0;
+  color: rgb(37, 37, 37);
   font-family: "Inter", sans-serif;
   font-weight: 300;
+  font-size: 24px;
+  text-transform: lowercase;
 }
 section {
+  padding: 0px 10px 400px 10px;
+}
+.sidebar {
+  width: 50px;
+  border-left: 4.4px solid rgb(37, 37, 37);
+  height: calc(100% - 20px);
+  margin: 10px 0;
+  writing-mode: vertical-lr;
+  line-height: 50px;
+}
+.cards {
   width: 100%;
   display: flex;
   flex-direction: row;
@@ -87,25 +193,51 @@ section {
   flex-wrap: wrap;
 }
 .card {
-  border: 1px solid grey;
   flex: none;
   width: 300px;
   margin: 30px 0;
   border-radius: 11px;
-  background: #f4f4f4;
-  box-shadow: -4px 4px 8px #dedede, 4px -4px 8px #ffffff;
+  background: #efefea;
+  border: 1.4px solid rgb(37, 37, 37);
+  translate: 0;
+  transition: all 0.25s;
+  &:hover {
+    box-shadow: -12px 12px 0px 0px rgb(37, 37, 37);
+    translate: 12px -12px;
+  }
+  & + & {
+    margin-left: 15px;
+  }
+}
+.card-header {
+  padding: 10px 20px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  border-bottom: 1.4px solid rgb(37, 37, 37);
 }
 .card-content {
   padding: 20px;
 }
+
 .project-description {
   margin: 12px 0 20px 0;
   font-weight: 300;
-  color: black;
+  color: rgb(37, 37, 37);
   font-family: "Inter", sans-serif;
 }
+.card-footer {
+  font-family: "Inter";
+  color: rgb(37, 37, 37);
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-content: center;
+  justify-content: flex-end;
+}
 .preview {
-  border-bottom: 1px solid black;
+  border-bottom: 1.4px solid rgb(37, 37, 37);
   height: 300px;
   width: 100%;
   display: flex;
@@ -117,8 +249,7 @@ section {
   height: 100%;
   width: 100%;
   object-fit: cover;
-  border-top-left-radius: 11px;
-  border-top-right-radius: 11px;
+  filter: grayscale(1);
 }
 .tools {
   display: flex;
@@ -128,20 +259,69 @@ section {
 }
 .tool {
   flex: none;
-  border-radius: 11px;
-  background: #f4f4f4;
-  box-shadow: -4px 4px 8px #dedede, 4px -4px 8px #ffffff;
-  border: 1px solid black;
-  padding: 5px 5px;
+  border-radius: 12px;
   display: inline-block;
   height: 25px;
-  margin: 0px 10px 10px 0;
+  filter: grayscale(1);
+  padding: 2px 15px 2px 12px;
+  margin: 2px;
+  border: 1.3px solid rgb(37, 37, 37);
 }
 .tool-name {
   font-size: 14px;
-  vertical-align: super;
-  color: black;
-  font-family: monospace;
-  font-weight: 100;
+  color: rgb(37, 37, 37);
+  font-weight: 300;
+}
+#contact {
+  background-color: rgb(37, 37, 37);
+  color: #efefea;
+  height: calc(100vh - 180px);
+  padding: 40px 40px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: flex-end;
+  align-items: flex-start;
+  width: 420px;
+}
+textarea {
+  background-color: rgb(37, 37, 37);
+  border: 1.4px solid #efefea;
+  border-radius: 6px;
+  color: #efefea;
+  font-family: "Inter";
+  padding: 10px;
+  resize: none;
+  height: 200px;
+  width: 400px;
+  margin: 10px 0;
+}
+input {
+  background-color: rgb(37, 37, 37);
+  border: 1.4px solid #efefea;
+  border-radius: 6px;
+  color: #efefea;
+  font-family: "Inter";
+  padding: 10px;
+  margin: 10px 0;
+}
+.submit-btn {
+  background-color: #efefea;
+  color: rgb(37, 37, 37);
+  border: 1.4px solid #efefea;
+  border-radius: 60px;
+  font-family: "Inter";
+  font-size: 14px;
+  padding: 10px 20px;
+  cursor: pointer;
+  margin-top: 10px;
+  align-self: flex-end;
+  font-weight: 800;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import './style.css';
 
 import { createApp } from 'vue';
 
+import { MotionPlugin } from '@vueuse/motion';
+
 import App from './App.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(MotionPlugin)
+app.mount('#app')
