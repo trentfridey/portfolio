8888ca1ed2f2037a1e5bd7f241495f8763c48f97
trentfridey <tfri.92@gmail.com>
2025-03-03 01:49:18 -0500

    fix deps

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import './style.css';
 import { createApp } from 'vue';
 
-import Vue3Marquee from 'vue3-marquee'
-
 import App from './App.vue';
 
 const app = createApp(App)
-app.use(Vue3Marquee)
 
 app.mount('#app')
